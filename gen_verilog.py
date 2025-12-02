89e275ef079b12bfe387b993bf39047d50504569
timmPrice <timmPrice6@gmail.com>
2025-12-02 15:56:05 -0500

    python row gen refactor working...

--- gen_verilog.py
+++ gen_verilog.py
@@ -42,61 +42,50 @@ def gen_verilog(circuit: Circuit):
         print("", file=file)
       
         # row logic
-        for i in range(circuit.p):
-            input, output = circuit.io[i] 
-            # input = input[::-1]
-            if input == "-" * circuit.n:
-                print(f"  wire r{i} = 1'b1;", file=file) 
+        rows = gen_row(circuit, file)
+        for i, bits in enumerate(rows):
+            if bits == []:
                 continue
+            if bits:
+                row = " && ".join(bits)
+                print(f"  wire r{i} = {row};", file=file)
             else:
-                conds = []
-                print(f"  wire r{i} = ", end="", file=file)
-                for j in range(circuit.n): 
-                    if input[j] == "0":
-                        conds.append(f"(x{j} == 0)")
-                    elif input[j] == "1":
-                        conds.append(f"(x{j} == 1)")
-                    else:
-                        continue
-                for j in range(len(conds)):
-                    print(f"{conds[j]}", end="", file=file)
-                    if j != len(conds) - 1:
-                        print(" && ", end="", file=file)
-                print(f";", file=file)
+                print(f"  wire r{i} = 1'b1;", file=file)
         print("", file=file)
 
         # output logic
-        outs = [[] for _ in range(circuit.m)]
-        for i, (input, output) in enumerate(circuit.io): 
-            for bit in range(circuit.m):
-                output = output[::-1]
-                if output == "-" * circuit.m:
-                    break
-                elif output[bit] == "1":
-                    outs[bit].append(f"r{i}")
 
-        for i in range(circuit.m):
-            ors = " | ".join(outs[i]) or "1'b0" 
-            print(f"  assign out[{i}] = {ors};", file=file)
+        # outs = [[] for _ in range(circuit.m)]
+        # for i, (input, output) in enumerate(circuit.io): 
+        #     for bit in range(circuit.m):
+        #         output = output[::-1]
+        #         if output == "-" * circuit.m:
+        #             break
+        #         elif output[bit] == "1":
+        #             outs[bit].append(f"r{i}")
+        #
+        # for i in range(circuit.m):
+        #     ors = " | ".join(outs[i]) or "1'b0" 
+        #     print(f"  assign out[{i}] = {ors};", file=file)
         print("", file=file)
 
         print("endmodule", file=file)
 
-def gen_row(circuit: Circuit, file) -> list[str]: 
-    conds: list[str] = []
+def gen_row(circuit: Circuit, file) -> list[list[str]]: 
+    rows: list[list[str]] = []
     for i, (inp, outp) in enumerate(circuit.io):
         if inp == "-" * circuit.n:
-            print(f"wire r{i} = 1'b1;")
+            print(f"wire r{i} = 1'b1;", file=file)
             continue;
-        else:
-            for j in range(circuit.n):
-                # print(f"  wire r{i} = ", end="", file=file)
-                if inp[j] == "1":
-                    conds.append(f"(x{j} == 1)")
-                elif inp[j] == "0":
-                    conds.append(f"(x{j} == 0)")
-
-    return conds
+        
+        conds: list[str] = []
+        for j in range(circuit.n):
+            if inp[j] == "1":
+                conds.append(f"(x{j} == 1)")
+            elif inp[j] == "0":
+                conds.append(f"(x{j} == 0)")
+        rows.append(conds)
+    return rows 
 
 def gen_assign(circuit: Circuit):
     print("")
@@ -106,7 +95,7 @@ def connect_row():
 
 def main():
     circuit = read_pla()
-    # gen_verilog(circuit)
+    gen_verilog(circuit)
     return 0
 
 if __name__ == "__main__":
--- out/circuit.v
+++ out/circuit.v
@@ -9,18 +9,17 @@ module circuit (
   wire x3 = in[3];
   wire x4 = in[4];
 
-  wire r0 = (x1 == 0) && (x3 == 1) && (x4 == 1);
-  wire r1 = (x0 == 1) && (x1 == 1) && (x4 == 1);
-  wire r2 = (x2 == 0) && (x4 == 1);
-  wire r3 = (x1 == 1) && (x4 == 1);
-  wire r4 = (x0 == 1) && (x4 == 1);
-  wire r5 = (x3 == 1);
-  wire r6 = 1'b1;
+  wire r0 = (x3 == 0) && (x4 == 1);
+  wire r1 = (x0 == 0) && (x2 == 0) && (x4 == 0);
+  wire r2 = (x1 == 0) && (x2 == 0);
+  wire r3 = (x2 == 1);
+  wire r4 = (x0 == 1);
+  wire r5 = (x0 == 0) && (x2 == 0);
 
-  assign out[0] = r1;
-  assign out[1] = r3 | r5;
-  assign out[2] = r6;
-  assign out[3] = r2 | r4;
-  assign out[4] = r0;
+  assign out[0] = r5;
+  assign out[1] = r2 | r5;
+  assign out[2] = r1;
+  assign out[3] = r3 | r4 | r5;
+  assign out[4] = r0 | r3 | r4;
 
 endmodule
